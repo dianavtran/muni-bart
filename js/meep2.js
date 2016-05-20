5de4f5798dece784aa27fc538d61c8cbb93d5e60
Diana Tran <git@dianatran.com>
2016-05-19 18:06:42 -0700

    :)

---
+++ js/meep2.js
@@ -0,0 +1,50 @@
+var fetch = function() {
+  $.ajax({
+    type: "GET",
+    url: "http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=J&stopId=17217",
+    dataType: "xml",
+    success: parseXml
+  });
+  $.ajax({
+    type: "GET",
+    url: "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=embr&key=MW9S-E7SL-26DU-VV8V",
+    dataType: "xml",
+    success: parseXmlBart
+  });
+  setInterval(fetch, 60000);
+  $('p').empty();
+}
+
+$(document).ready(function() {
+  fetch();
+});
+
+function parseXml(xml)
+{
+  var muniArr = [];
+  $(xml).find("prediction").each(function()
+  {
+    muniArr.push($(this).attr('minutes'));
+  });
+  $("#j").html("<p>"+ muniArr.sort(function(a, b){return a-b}).join(',​​​​')+"</p>");
+  clearInterval(fetch);
+  fetch = null;
+
+}
+
+function parseXmlBart(xml)
+{
+  var bartArr = [];
+  $(xml).find("estimate").each(function()
+  {
+    if ($(this).find("direction").text() == "North") {
+      if ($(this).siblings().find('minutes').html() != "Leaving" && $(this).siblings().find('minutes').html() > 5) {
+    bartArr.push($(this).siblings().find('minutes').html());
+      }
+    }
+  });
+  var bartValues = bartArr.slice(0,6).sort(function(a, b){return a-b});
+  $("#bart").html("<p>"+ bartValues.join(',​​​​') +"</p>");
+  clearInterval(fetch);
+  fetch = null;
+}
